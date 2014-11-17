842b9924834adcd1e210cc3e5b3add0e8367f997
yamadapc <tacla.yamada@gmail.com>
2014-11-17 09:28:42 -0500

    Fix bugs and improve the existing parsing API
    This adds basic `example` parsing, improves the tests and fixes bugs
    with `comment-parser` module.

--- test/comment-parser.test.js
+++ test/comment-parser.test.js
@@ -3,20 +3,33 @@ require('should');
 var commentParser = require('../lib/comment-parser');
 
 describe('comment-parser', function() {
-  describe('.run(input)', function() {
+  describe('.parseComments(input)', function() {
     it('differentiates code blocks from comments', function() {
-      commentParser.run(
-        "function something() {"                  +
-        "/* multi-line comment here\n"            +
-        "                          */\n"          +
-        "}\n"                                     +
-        "\n"                                      +
-        "something() // single-line comment here"
+      commentParser.parseComments(
+        'function something() {'                  +
+        '/* multi-line comment here\n'            +
+        '                          */\n'          +
+        '}\n'                                     +
+        '\n'                                      +
+        'something() // single-line comment here'
       ).should.eql([
         { type: 'code', string: 'function something() {', },
         { type: 'comment', string: 'multi-line comment here', },
         { type: 'code', string: '}\n\nsomething()', },
         { type: 'comment', string: 'single-line comment here', },
+      ]);
+    });
+  });
+
+  describe('.parseExamples(parsedComments)', function() {
+    it('extracts examples out of parsed comments', function() {
+      commentParser.parseExamples(commentParser.parseComments(
+        'a()\n' +
+        '// ignored\n'  +
+        'b()\n' +
+        '// => 20'
+      )).should.eql([
+        { testCase: 'b()', expectedResult: '20', },
       ]);
     });
   });
