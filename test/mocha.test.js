2b57ab9af0249094ead35ab3daafc1b1315c0943
Dmitry Mazurok <mazurok@outlook.com>
2019-06-26 22:38:03 +0700

    Add support for multiline results #35

--- test/mocha.test.js
+++ test/mocha.test.js
@@ -77,5 +77,34 @@ describe('jsdoctest/mocha', function() {
 
       called.should.be.true;
     });
+
+    it('handles multiline examples', function() {
+      var called = false;
+      var mockModule = {
+        _compile: onCompile
+      };
+
+      mocha.loadDoctests(mockModule, path.join(__dirname, './multiline-file.js'));
+
+      function onCompile(content, filename) {
+        content.should.containEql(
+          '\ndescribe(\'subtract()\', function() {'+
+              'it(\'subtract(1, 2)\', function() {'+
+                '(subtract(1, 2)).should.eql({ normal: -1, reverse: 1 });'+
+              '});\n' +
+              'it(\'subtract(1, 2)\', function() {'+
+                '(subtract(1, 2)).should.eql({  normal: -1,  reverse: 1});'+
+              '});\n' +
+              'it(\'subtract(3, 6)\', function() {'+
+                '(subtract(3, 6)).should.eql({ normal: -3, reverse: 3 });'+
+              '});'+
+            '});'
+        );  
+        called = true;
+        filename.should.equal(path.join(__dirname, 'multiline-file.js'));
+      }
+
+      called.should.be.true;
+    });    
   });
 });
